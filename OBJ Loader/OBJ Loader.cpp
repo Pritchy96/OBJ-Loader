081045d70bfe9d886cc6f110e03530c2f03010ee
Tom Pritchard <tompritchard1996@gmail.com>
2014-07-05 00:42:44 +0100

    Various changes, can now deal with any shape face

--- OBJ Loader/OBJ Loader.cpp
+++ OBJ Loader/OBJ Loader.cpp
@@ -41,9 +41,13 @@ double rotate_x=0;
 double rotate_z=0;
 double scale = 0.5;
 
+static float lightPos[] = {1.0, 0.0, 1.0, 1.0}; 
+
 //2D vectors holding vert and poly data.
 vector<vector<string>> strVcrVerts;
 vector<vector<string>> strVcrPolys;
+
+bool debug = false;
 
 //	-------------------------------------------------------
 // display() Callback function
@@ -74,40 +78,21 @@ void display(){
 		int vertToDraw = 0;
 
 		glBegin(GL_POLYGON);
-		glColor3f( 0, 0.5, 0.0 );  
 
-		//Finds the row of the vert to draw in strArrVerts by finding which number strArrPoly is looking for.
-		vertToDraw = (int) atof(strVcrPolys[i][0].c_str()) - 1;
-		//Draws Vertex, getting the coords from strArrVerts.
-		glVertex3f( 
-			(float) atof(strVcrVerts[vertToDraw][0].c_str()), 
-			(float) atof(strVcrVerts[vertToDraw][1].c_str()), 
-			(float) atof(strVcrVerts[vertToDraw][2].c_str()) 
-			);      
+		for (int j = 0; j < strVcrPolys[i].size(); j++)
+		{
+			glColor3f( 1/(j - 0.5), 1/(0.4 + j), 1/(j - 0.3) );  
 
-		glColor3f( 0, 0, 0.6 );  
-		vertToDraw = (int) atof(strVcrPolys[i][1].c_str()) - 1;
-		glVertex3f(
-			(float) atof(strVcrVerts[vertToDraw][0].c_str()), 
-			(float) atof(strVcrVerts[vertToDraw][1].c_str()), 
-			(float) atof(strVcrVerts[vertToDraw][2].c_str()) 
-			);   
+			//Finds the row of the vert to draw in strArrVerts by finding which number strArrPoly is looking for.
+			vertToDraw = (int) atof(strVcrPolys[i][j].c_str()) - 1;
+			//Draws Vertex, getting the coords from strArrVerts.
+			glVertex3f( 
+				(float) atof(strVcrVerts[vertToDraw][0].c_str()), 
+				(float) atof(strVcrVerts[vertToDraw][1].c_str()), 
+				(float) atof(strVcrVerts[vertToDraw][2].c_str()) 
+				);    
+		}
 
-		glColor3f( 0.2, 0, 0.0 );  
-		vertToDraw = (int) atof(strVcrPolys[i][2].c_str()) - 1;
-		glVertex3f(
-			(float) atof(strVcrVerts[vertToDraw][0].c_str()), 
-			(float) atof(strVcrVerts[vertToDraw][1].c_str()), 
-			(float) atof(strVcrVerts[vertToDraw][2].c_str()) 
-			);     
-
-		glColor3f( 0.5, 0, 0.5 );  
-		vertToDraw = (int) atof(strVcrPolys[i][3].c_str()) - 1;
-		glVertex3f(
-			(float) atof(strVcrVerts[vertToDraw][0].c_str()), 
-			(float) atof(strVcrVerts[vertToDraw][1].c_str()), 
-			(float) atof(strVcrVerts[vertToDraw][2].c_str()) 
-			);     
 
 		glEnd();
 	}
@@ -138,7 +123,7 @@ void specialKeys( int key, int x, int y ) {
 		rotate_x -= 5;
 		break;
 	case (GLUT_KEY_PAGE_UP):
-		scale += 0.01;
+		scale += 0.05;
 		break;
 	case (GLUT_KEY_PAGE_DOWN):
 		scale -= 0.01;
@@ -174,21 +159,33 @@ int main(int argc, char* argv[]){
 	glutCreateWindow("OBJ Loader");
 	// glutFullScreen();
 
-	//Resize Window
-	// glutReshapeWindow(700, 700);
-
 	//Enable Z-buffer depth test. Draws polys in 3d space: 
 	//no overlap with polys in front/behind (last drawn polies would be on top)
 	glEnable(GL_DEPTH_TEST);
 
+	
+		
 	//Only render polys facing the viewport.
-	glEnable(GL_CULL_FACE);
+	//glEnable(GL_CULL_FACE);
 	//Change cull style.
-	glCullFace(GL_FRONT);
+	//glCullFace(GL_FRONT);
 
 	//Lighting
 	//glEnable(GL_LIGHTING);
-	//glEnable(GL_LIGHT1);
+	glEnable(GL_LIGHT0);
+
+	// Create light components.
+	GLfloat ambientLight[] = { 0.2f, 0.2f, 0.2f, 1.0f };
+	GLfloat diffuseLight[] = { 0.8f, 0.8f, 0.8, 1.0f };
+	GLfloat specularLight[] = { 0.5f, 0.5f, 0.5f, 1.0f };
+	GLfloat position[] = { 0.0f, 2.0f, 1.0f, 1.0f };
+
+	// Assign created components to GL_LIGHT0.
+	glLightfv(GL_LIGHT0, GL_AMBIENT, ambientLight);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, diffuseLight);
+	glLightfv(GL_LIGHT0, GL_SPECULAR, specularLight);
+	glLightfv(GL_LIGHT0, GL_POSITION, position);
+	
 
 
 	// Callback functions
@@ -207,7 +204,7 @@ int main(int argc, char* argv[]){
 //	-------------------------------------------------------
 void ReadFile()
 {
-	ifstream file ("cube.obj");
+	ifstream file ("torus knot HD.obj");
 
 	string currentLine = "";
 
@@ -243,14 +240,19 @@ void ReadFile()
 					}
 				}
 
+				//Removes the empty string that was added but not filled.
+				split.pop_back();
 				//Adds the finished 'split' to end of strVcrVerts.
 				strVcrVerts.push_back(split);
 
 				//Debug.
-				cout << "Vert" << endl;
-				cout << strVcrVerts[vertIncrement][0] <<  endl;
-				cout << strVcrVerts[vertIncrement][1] << endl;
-				cout << strVcrVerts[vertIncrement][2] << "\n" << endl;
+				if (debug)
+				{
+					cout << "Vert" << endl;
+					cout << strVcrVerts[vertIncrement][0] <<  endl;
+					cout << strVcrVerts[vertIncrement][1] << endl;
+					cout << strVcrVerts[vertIncrement][2] << "\n" << endl;
+				}
 
 				//Increment vertIncrement so we put the next split in the next array row.
 				vertIncrement++;
@@ -275,15 +277,21 @@ void ReadFile()
 					}
 				}
 
+				//Removes the empty string that was added but not filled.
+				split.pop_back();
 				//Adds the finished 'split' to end of strVcrVerts.
 				strVcrPolys.push_back(split);
 
 				//Debug.
-				cout << "Poly" << endl;
-				cout << strVcrPolys[polyIncrement][0] <<  endl;
-				cout << strVcrPolys[polyIncrement][1] <<  endl;
-				cout << strVcrPolys[polyIncrement][2] << endl;
-				cout << strVcrPolys[polyIncrement][3] << "\n" << endl;
+				if (debug)
+				{
+					cout << "Poly" << endl;
+					for (int j = 0; j < strVcrPolys[polyIncrement].size(); j++)
+					{
+						cout << strVcrPolys[polyIncrement][j] <<  endl;
+					}
+					cout << "\n" << endl;
+				}
 
 				//Increment vertIncrement so we put the next split in the next array row.
 				polyIncrement++;
@@ -298,57 +306,3 @@ void ReadFile()
 }
 
 
-/*2D Vector Initialisation and resizing.
-#pragma region VECTOR_TESTING
-int num_of_col = 5;
-int num_of_row = 9;
-double init_value = 0;
-
-vector< vector<String> > strPolyVcr;
-//now we have an empty 2D-matrix of size (0,0). Resizing it with one single command:
-strPolyVcr.resize( num_of col , vector<double>( num_of_row , init_value ) );
-// and we are good to go ...  
-
-strPolyVcr.insert ( 1 , "200" );
-
-//Replace Array with this.
-vector< vector<String> > strVcrVerts;
-
-stringstream stream(currentLine);	//Allows String Manipulation
-int j = 0;	//Row Iterator for both String Arrays.
-
-#pragma region ADDING_VERTICIES
-//If it finds an v at position 0 of the string (line begins with a v)
-if(currentLine.find("v") == 0) 
-{
-//Reciprocal for split value for inserting into Vector.
-string split = '';
-//Split String into string array, if there is a new line.
-while( getline(stream, split, ' ') )
-{
-//Insert 'split' into Vector at the correct position.
-strVcrVerts.insert(strVcrVerts[vertIncrement][j], split)
-//Increment j so we put the next value in the next column.
-j++;
-}
-//Debug.
-cout << "Vert" << endl;
-cout << strVcrVerts[vertIncrement][2] <<  endl;
-cout << strVcrVerts[vertIncrement][3] << endl;
-cout << strVcrVerts[vertIncrement][4] << "\n" << endl;
-
-//Increment vertIncrement so we put the next split in the next array row.
-vertIncrement++;
-}
-#pragma endregion
-
-//Vert Drawing Example.
-glVertex3f( 
-(float) atof(strVcrVerts[vertToDraw][2].c_str()), 
-(float) atof(strVcrVerts[vertToDraw][3].c_str()), 
-(float) atof(strVcrVerts[vertToDraw][4].c_str()) 
-); 
-
-#pragma endregion
-
-*/

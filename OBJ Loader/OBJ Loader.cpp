88b607a81aad063d25d5a3dc3cb029507a1408f3
Tom Pritchard <tompritchard1996@gmail.com>
2014-09-25 23:15:34 +0100

    Comments, nicer colours.

--- OBJ Loader/OBJ Loader.cpp
+++ OBJ Loader/OBJ Loader.cpp
@@ -41,7 +41,7 @@ int wasMain();
 double rotate_y=0;
 double rotate_x=0;
 double rotate_z=0;
-double scale = 1;
+double scale = 0.01;
 
 static float lightPos[] = {1.0, 0.0, 1.0, 1.0}; 
 
@@ -53,12 +53,12 @@ bool debug = false;
 void display(){
 
 
-	#pragma region LOADING_IMAGE
-	 BMP heightmap;
-	 heightmap.ReadFromFile("HeightMap.bmp");
-	#pragma endregion
-	
-	#pragma region SET_UP_OPENGL.
+#pragma region LOADING_IMAGE
+	BMP heightmap;
+	heightmap.ReadFromFile("HeightMap.bmp");
+#pragma endregion
+
+#pragma region SET_UP_OPENGL.
 	//  Clear screen and Z-buffer
 	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 
@@ -75,64 +75,77 @@ void display(){
 
 	// Other Transformations
 	glScalef( scale, scale, scale ); 
-	#pragma endregion
+#pragma endregion
 
 	//Iterate through bitmap
-	for (int i = -(heightmap.TellWidth() / 2); i < (heightmap.TellWidth()/2); i++)
+	for (int i = -(heightmap.TellWidth() / 2); i < (heightmap.TellWidth()/2); i++)	//Goes from -100 to + 100 rather than 0 - 200 so camera is centered.
 	{
-		for (int j = -(heightmap.TellHeight() / 2); (j < heightmap.TellHeight() / 2); j++)
+		for (int j = -(heightmap.TellHeight() / 2); (j < heightmap.TellHeight() / 2); j++) //As above. This means some statements below use (+ heightmap.TellHeight()/2) to make it 0 - 200 again.
 		{
-		//Pixel Height/RBG Value
-		int pixelHeight = heightmap.GetPixel(i + (heightmap.TellWidth()/2), j + (heightmap.TellHeight()/2)).Blue;
+			int iFromZero = i + (heightmap.TellWidth()/2);
+			int jFromZero = j + (heightmap.TellHeight()/2);
 
-		glBegin(GL_POLYGON);	//Must be called for each face.
-	
+			//Pixel Height/RBG Value
+			int pixelHeight = heightmap.GetPixel(iFromZero, jFromZero).Blue;
+
+			glBegin(GL_POLYGON);	//Must be called for each face.
+
 			//X = Width = i
 			//Y = Height = pixelHeight
 			//Z = Depth = j.
-			
+
+			float colour1 = ((float)(pixelHeight) / 255) * 1;
+			float colour2 = 1 / ( (float) jFromZero/ (float)heightmap.TellHeight());
+
+			glColor3f( colour1, colour1, colour1);  //Colour so faces can be differentiated.
 			float y = ((float) pixelHeight) / 10;
 
-			//Top Left (From Topdown)
-			glColor3f( 1/(0 - 0.5), 1/(0.4 + 0), 1/(0 - 0.3) );  //Colour so faces can be differentiated.
-			//Draws Vertex, getting the coords from strArrVerts.
-			glVertex3f( 
+			#pragma region TOP_FACE
+			glVertex3f(		//Top Left
 				(float) (i), 
 				y, 
 				(float) (j) 
 				);    
-				
-			//Top Right (From Topdown)
-			glColor3f( 1/(1 - 0.5), 1/(0.4 + 1), 1/(1 - 0.3) );  //Colour so faces can be differentiated.
-			//Draws Vertex, getting the coords from strArrVerts.
-			glVertex3f( 
+			glVertex3f(		//Top Right
 				(float) ((i + 1)), 
 				y, 
 				(float) (j) 
 				);    
-				
-		
-
-			//Bottom Right (From Topdown)
-			glColor3f( 1/(3 - 0.5), 1/(0.4 + 3), 1/(3 - 0.3) );  //Colour so faces can be differentiated.
-			//Draws Vertex, getting the coords from strArrVerts.
-			glVertex3f( 
+			glVertex3f(		//Bottom Right
 				(float) ((i + 1)), 
 				y, 
 				(float) ((j + 1)) 
 				);  
-
-						//Bottom Left (From Topdown)
-			glColor3f( 1/(2 - 0.5), 1/(0.4 + 2), 1/(2 - 0.3) );  //Colour so faces can be differentiated.
-			//Draws Vertex, getting the coords from strArrVerts.
-			glVertex3f( 
+			glVertex3f(		//Bottom Left
 				(float) (i), 
 				y, 
 				(float) ((j + 1)) 
 				);    
-				
+			#pragma endregion
 
-		glEnd();
+			#pragma region TOP_FACE
+			glVertex3f(		//Top Left
+				(float) (i), 
+				y, 
+				(float) (j) 
+				);    
+			glVertex3f(		//Top Right
+				(float) ((i + 1)), 
+				y, 
+				(float) (j) 
+				);    
+			glVertex3f(		//Bottom Right
+				(float) ((i + 1)), 
+				y, 
+				(float) ((j + 1)) 
+				);  
+			glVertex3f(		//Bottom Left
+				(float) (i), 
+				y, 
+				(float) ((j + 1)) 
+				);    
+			#pragma endregion
+			glEnd();
 		}
 	}
 
@@ -197,7 +210,7 @@ int main(int argc, char* argv[]){
 	//Enable Z-buffer depth test. Draws polys in 3d space: 
 	//no overlap with polys in front/behind (last drawn polies would be on top)
 	glEnable(GL_DEPTH_TEST);
-	
+
 	//Only render polys facing the viewport.
 	//glEnable(GL_CULL_FACE);
 	//Change cull style.
@@ -236,105 +249,105 @@ int main(int argc, char* argv[]){
 //	-------------------------------------------------------
 void ReadFile()
 {
-	ifstream file ("torus knot HD.obj");
+ifstream file ("torus knot HD.obj");
 
-	string currentLine = "";
+string currentLine = "";
 
-	if (file.is_open())
-	{
-		int vertIncrement = 0;
-		int polyIncrement = 0;
+if (file.is_open())
+{
+int vertIncrement = 0;
+int polyIncrement = 0;
 
-		//While we are not at the end of a file (gets the line from file if not)
-		while ( getline (file, currentLine) )
-		{
-			stringstream stream(currentLine);		//Allows String Manipulation
-			int j = 0;		//Column Iterator for both String Arrays.
+//While we are not at the end of a file (gets the line from file if not)
+while ( getline (file, currentLine) )
+{
+stringstream stream(currentLine);		//Allows String Manipulation
+int j = 0;		//Column Iterator for both String Arrays.
 
-			vector<string> split ;	//Reciprocal for split value for inserting into Vector.
-			split.resize(1, "");
+vector<string> split ;	//Reciprocal for split value for inserting into Vector.
+split.resize(1, "");
 
-			#pragma region ADDING_VERTICIES
-			//If it finds an v at position 0 of the string (line begins with a v)
-			if(currentLine.find("v") == 0) 
-			{
-				//Split String into string array, if there is a new line.
-				while( getline(stream, split[j], ' ') )
-				{
-					//If it's empty or a v, don't increment so it can be overwritten.
-					if (!split[j].empty() && 
-						!split[j].find("v") == 0)
-					{
-						//Add a new cell to split to be written to.
-						split.push_back("");
-						//Increment j so we put the next value in the next column.
-						j++;
-					}
-				}
+#pragma region ADDING_VERTICIES
+//If it finds an v at position 0 of the string (line begins with a v)
+if(currentLine.find("v") == 0) 
+{
+//Split String into string array, if there is a new line.
+while( getline(stream, split[j], ' ') )
+{
+//If it's empty or a v, don't increment so it can be overwritten.
+if (!split[j].empty() && 
+!split[j].find("v") == 0)
+{
+//Add a new cell to split to be written to.
+split.push_back("");
+//Increment j so we put the next value in the next column.
+j++;
+}
+}
 
-				//Removes the empty string that was added but not filled.
-				split.pop_back();
-				//Adds the finished 'split' to end of strVcrVerts.
-				strVcrVerts.push_back(split);
+//Removes the empty string that was added but not filled.
+split.pop_back();
+//Adds the finished 'split' to end of strVcrVerts.
+strVcrVerts.push_back(split);
 
-				//Debug.
-				if (debug)
-				{
-					cout << "Vert" << endl;
-					cout << strVcrVerts[vertIncrement][0] <<  endl;
-					cout << strVcrVerts[vertIncrement][1] << endl;
-					cout << strVcrVerts[vertIncrement][2] << "\n" << endl;
-				}
+//Debug.
+if (debug)
+{
+cout << "Vert" << endl;
+cout << strVcrVerts[vertIncrement][0] <<  endl;
+cout << strVcrVerts[vertIncrement][1] << endl;
+cout << strVcrVerts[vertIncrement][2] << "\n" << endl;
+}
 
-				//Increment vertIncrement so we put the next split in the next array row.
-				vertIncrement++;
-			}
-			#pragma endregion
+//Increment vertIncrement so we put the next split in the next array row.
+vertIncrement++;
+}
+#pragma endregion
 
-			#pragma region ADDING_POLYGONS
-			//If it finds an f at position 0 of the string (line begins with a v)
-			else if(currentLine.find("f") == 0) 
-			{
-				//Split String into string array, if there is a new line.
-				while( getline(stream, split[j], ' ') )
-				{
-					//If it's empty or a v, don't increment so it can be overwritten.
-					if (!split[j].empty() && 
-						!split[j].find("f") == 0)
-					{
-						//Add a new cell to split to be written to.
-						split.push_back("");
-						//Increment j so we put the next value in the next column.
-						j++;
-					}
-				}
+#pragma region ADDING_POLYGONS
+//If it finds an f at position 0 of the string (line begins with a v)
+else if(currentLine.find("f") == 0) 
+{
+//Split String into string array, if there is a new line.
+while( getline(stream, split[j], ' ') )
+{
+//If it's empty or a v, don't increment so it can be overwritten.
+if (!split[j].empty() && 
+!split[j].find("f") == 0)
+{
+//Add a new cell to split to be written to.
+split.push_back("");
+//Increment j so we put the next value in the next column.
+j++;
+}
+}
 
-				//Removes the empty string that was added but not filled.
-				split.pop_back();
-				//Adds the finished 'split' to end of strVcrVerts.
-				strVcrPolys.push_back(split);
+//Removes the empty string that was added but not filled.
+split.pop_back();
+//Adds the finished 'split' to end of strVcrVerts.
+strVcrPolys.push_back(split);
 
-				//Debug.
-				if (debug)
-				{
-					cout << "Poly" << endl;
-					for (int j = 0; j < strVcrPolys[polyIncrement].size(); j++)
-					{
-						cout << strVcrPolys[polyIncrement][j] <<  endl;
-					}
-					cout << "\n" << endl;
-				}
+//Debug.
+if (debug)
+{
+cout << "Poly" << endl;
+for (int j = 0; j < strVcrPolys[polyIncrement].size(); j++)
+{
+cout << strVcrPolys[polyIncrement][j] <<  endl;
+}
+cout << "\n" << endl;
+}
 
-				//Increment vertIncrement so we put the next split in the next array row.
-				polyIncrement++;
-			}
-			#pragma endregion
-		}
-		//We now have a completed file load.
-		file.close();
-	}
+//Increment vertIncrement so we put the next split in the next array row.
+polyIncrement++;
+}
+#pragma endregion
+}
+//We now have a completed file load.
+file.close();
+}
 
-	else cout << "Unable to open file"; 
+else cout << "Unable to open file"; 
 }
 */
 
@@ -345,86 +358,86 @@ void ReadFile()
 void display(){
 
 
-	#pragma region LOADING_IMAGE
-	 BMP heightmap;
-	 heightmap.ReadFromFile("heightmap.bmp");
-	#pragma endregion
-	
-	#pragma region SET_UP_OPENGL.
-	//  Clear screen and Z-buffer
-	glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+#pragma region LOADING_IMAGE
+BMP heightmap;
+heightmap.ReadFromFile("heightmap.bmp");
+#pragma endregion
 
-	// Reset transformations
-	glLoadIdentity();
+#pragma region SET_UP_OPENGL.
+//  Clear screen and Z-buffer
+glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
 
-	// Other Transformations
-	// glTranslatef( 0.1, 0.0, 0.0 );
+// Reset transformations
+glLoadIdentity();
 
-	// Rotation.
-	glRotatef( rotate_x, 1.0, 0.0, 0.0 );
-	glRotatef( rotate_y, 0.0, 1.0, 0.0 );
-	glRotatef(rotate_z, 0.0, 0.0, 1.0);
+// Other Transformations
+// glTranslatef( 0.1, 0.0, 0.0 );
 
-	// Other Transformations
-	glScalef( scale, scale, scale ); 
-	#pragma endregion
+// Rotation.
+glRotatef( rotate_x, 1.0, 0.0, 0.0 );
+glRotatef( rotate_y, 0.0, 1.0, 0.0 );
+glRotatef(rotate_z, 0.0, 0.0, 1.0);
 
-	//Iterate through bitmap
-	for (int i = 0; i < heightmap.TellWidth(); i++)
-	{
-		for (int j = 0; j < heightmap.TellHeight(); j++)
-		{
-		//Pixel Height/RBG Value
-		int pixelHeight = heightmap.GetPixel(i, j).Blue;
+// Other Transformations
+glScalef( scale, scale, scale ); 
+#pragma endregion
 
-		glBegin(GL_POLYGON);	//Must be called for each face.
-	
-			//X = Width = i
-			//Y = Height = pixelHeight
-			//Z = Depth = j.
-			
-			
-			//Top Left (From Topdown)
-			glColor3f( 1/(0 - 0.5), 1/(0.4 + 0), 1/(0 - 0.3) );  //Colour so faces can be differentiated.
-			//Draws Vertex, getting the coords from strArrVerts.
-			glVertex3f( 
-				(float) (i * 10), 
-				(float) (pixelHeight), 
-				(float) (j * 10) 
-				);    
-				
-			//Top Right (From Topdown)
-			glColor3f( 1/(1 - 0.5), 1/(0.4 + 1), 1/(1 - 0.3) );  //Colour so faces can be differentiated.
-			//Draws Vertex, getting the coords from strArrVerts.
-			glVertex3f( 
-				(float) ((i + 1) * 10), 
-				(float) (pixelHeight), 
-				(float) (j * 10) 
-				);    
-				
-			//Bottom Left (From Topdown)
-			glColor3f( 1/(2 - 0.5), 1/(0.4 + 2), 1/(2 - 0.3) );  //Colour so faces can be differentiated.
-			//Draws Vertex, getting the coords from strArrVerts.
-			glVertex3f( 
-				(float) (i * 10), 
-				(float) (pixelHeight), 
-				(float) ((j + 1) * 10) 
-				);    
-				
-			//Bottom Right (From Topdown)
-			glColor3f( 1/(3 - 0.5), 1/(0.4 + 3), 1/(3 - 0.3) );  //Colour so faces can be differentiated.
-			//Draws Vertex, getting the coords from strArrVerts.
-			glVertex3f( 
-				(float) ((i + 1) * 10), 
-				(float) (pixelHeight), 
-				(float) ((j + 1) * 10) 
-				);   
+//Iterate through bitmap
+for (int i = 0; i < heightmap.TellWidth(); i++)
+{
+for (int j = 0; j < heightmap.TellHeight(); j++)
+{
+//Pixel Height/RBG Value
+int pixelHeight = heightmap.GetPixel(i, j).Blue;
 
-		glEnd();
-		}
-	}
+glBegin(GL_POLYGON);	//Must be called for each face.
 
-	glFlush();
-	glutSwapBuffers();
+//X = Width = i
+//Y = Height = pixelHeight
+//Z = Depth = j.
+
+
+//Top Left (From Topdown)
+glColor3f( 1/(0 - 0.5), 1/(0.4 + 0), 1/(0 - 0.3) );  //Colour so faces can be differentiated.
+//Draws Vertex, getting the coords from strArrVerts.
+glVertex3f( 
+(float) (i * 10), 
+(float) (pixelHeight), 
+(float) (j * 10) 
+);    
+
+//Top Right (From Topdown)
+glColor3f( 1/(1 - 0.5), 1/(0.4 + 1), 1/(1 - 0.3) );  //Colour so faces can be differentiated.
+//Draws Vertex, getting the coords from strArrVerts.
+glVertex3f( 
+(float) ((i + 1) * 10), 
+(float) (pixelHeight), 
+(float) (j * 10) 
+);    
+
+//Bottom Left (From Topdown)
+glColor3f( 1/(2 - 0.5), 1/(0.4 + 2), 1/(2 - 0.3) );  //Colour so faces can be differentiated.
+//Draws Vertex, getting the coords from strArrVerts.
+glVertex3f( 
+(float) (i * 10), 
+(float) (pixelHeight), 
+(float) ((j + 1) * 10) 
+);    
+
+//Bottom Right (From Topdown)
+glColor3f( 1/(3 - 0.5), 1/(0.4 + 3), 1/(3 - 0.3) );  //Colour so faces can be differentiated.
+//Draws Vertex, getting the coords from strArrVerts.
+glVertex3f( 
+(float) ((i + 1) * 10), 
+(float) (pixelHeight), 
+(float) ((j + 1) * 10) 
+);   
+
+glEnd();
+}
+}
+
+glFlush();
+glutSwapBuffers();
 }
 */
